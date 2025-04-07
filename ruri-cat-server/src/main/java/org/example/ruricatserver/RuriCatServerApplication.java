1f6cef40a5c201d3bff92f518478b0c2312be373
zdx-1 <19546328912@163.com>
2025-04-07 12:50:46 +0800

    添加后端项目

---
+++ ruri-cat-server/src/main/java/org/example/ruricatserver/RuriCatServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.ruricatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuriCatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuriCatServerApplication.class, args);
+    }
+
+}
---
+++ ruri-cat-server/src/test/java/org/example/ruricatserver/RuriCatServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.ruricatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuriCatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World");
+
+    }
+
+}
